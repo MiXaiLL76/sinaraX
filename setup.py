b7ed5b37cc2e44e125e5f2e5068e515c9c09110c
MiXaiLL76 <mike.milos@yandex.ru>
2024-03-05 00:54:54 +0300

    format lines

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ def get_version():
         exec(compile(f.read(), version_file, "exec"))
     return locals()["__version__"], locals()["__author__"]
 
+
 def readme():
     with open("README.md", encoding="utf-8") as f:
         content = f.read()
@@ -86,6 +87,7 @@ def parse_requirements(fname="requirements/runtime.txt", with_version=True):
 
     packages = list(gen_packages_items())
     return packages
+
 
 __version__, __author__ = get_version()
 
